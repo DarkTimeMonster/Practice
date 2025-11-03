05bac24998090835477c040efa9ae6f0b0d0a9b8
DarkTimeMonster <dawiskibamaksim@gmail.com>
2025-11-03 16:46:17 +0200

    del

--- wwwroot/js/shared_script.js
+++
@@ -1,9 +0,0 @@
-﻿window.addEventListener('scroll', () => {
-    const header = document.querySelector('.main-header');
-    if (window.scrollY > 100) {
-        header.style.background = 'rgba(21, 101, 192, 0.9)';
-        header.style.transition = '0.4s';
-    } else {
-        header.style.background = 'linear-gradient(180deg, #e3f2fd, #ffffff)';
-    }
-});
